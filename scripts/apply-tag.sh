41b74eb4b4f2376450b4399b567bf9d6798fd880
Tyler Hebenstreit <tyler.hebenstreit@breaktech.com>
2024-09-29 19:11:56 -0500

    Removed unused refs

--- scripts/apply-tag.sh
+++ scripts/apply-tag.sh
@@ -2,4 +2,4 @@
 # This script updates the version number for
 # the addon based on the current commit timestamp
 TAG_NAME=${TAG_NAME:-$(git -c "core.abbrev=8" show -s "--format=%cd-%h" "--date=format:%Y.%m.%d")}
-sed -i "s/version:.*/version: \"$TAG_NAME\"/" addon/config.yaml
+sed -i'' -e "s/version:.*/version: \"$TAG_NAME\"/" addon/config.yaml
